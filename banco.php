e8bb4f585dae7a15774174929f5d648764ce8fd3
Igor Teles Caetano <igor.caetano@123milhas.com.br>
2023-05-23 16:57:59 -0300

    finaliza curso php avancado

--- banco.php
+++ banco.php
@@ -40,10 +40,34 @@ unset($contasCorrentes['454.654.578-92']); //remove da memoria as informações de
 
 titularComLetrasMaiusculas($contasCorrentes['123.456.789-10']);
 
-echo "<ul>"; //inicia uma lista em HTML
-foreach ($contasCorrentes as $cpf => $conta) { //$cpf => $conta é o mesmo que dizer: indice => valor. Ou seja, no foreach, o indice não é definido
-    ['titular' => $titular, 'saldo' => $saldo] = $conta; //estou passando meu indice titular pra uma variavel titular e meu indice saldo para uma variavel chamada saldo e armazenando ambos na variavel conta, tudo dentro do list []
-    exibeConta($conta);
-}
-echo "</ul>"; //fecha a lista em html
+// echo "<ul>"; //inicia uma lista em HTML
+// foreach ($contasCorrentes as $cpf => $conta) { //$cpf => $conta é o mesmo que dizer: indice => valor. Ou seja, no foreach, o indice não é definido
+//     ['titular' => $titular, 'saldo' => $saldo] = $conta; //estou passando meu indice titular pra uma variavel titular e meu indice saldo para uma variavel chamada saldo e armazenando ambos na variavel conta, tudo dentro do list []
+//     exibeConta($conta);
+// }
+// echo "</ul>"; //fecha a lista em html
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contas correntes</h1>
+    <dl>
+        <?php foreach ($contasCorrentes as $cpf => $conta) { ?> <!--aqui eu abro e fecho o PHP para reabri-lo na linha 69 e fechalo. É a interpolação entre php e html -->
+        <dt>
+            <h3><?= $conta['titular']; ?> - <?= $cpf; ?></h3> <!--faço o mesmo aqui, abrindo o php dentro do html, só de colocar "menor?=, ele entende que é um php, também não preciso colocar o echo-->
+        </dt>
+        <dd>
+            Saldo: <?= $conta['saldo']; ?>
+        </dd> <!--dado dessa lista-->
+        <?php } ?> 
+    </dl>  
+</body>
+</html>
+
